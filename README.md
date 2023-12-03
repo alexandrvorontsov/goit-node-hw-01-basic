b41a00e49cf07abd4d00a501f6bc35a03e1229db
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2023-12-03 21:46:00 +0200

    add

---
+++ README.md
@@ -0,0 +1,22 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list"
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+
+# Cкріншоти виконання команд
+
+1. https://ibb.co/9VLrpYH
+2. https://ibb.co/FYMszsj
+3. https://ibb.co/8r8s2VC
+4. https://ibb.co/MRsVYrr
---
+++ contacts.js
@@ -0,0 +1,56 @@
+const fs = require("fs").promises;
+const path = require("path");
+const uuid = require("uuid");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function writeNewContacts(data) {
+  try {
+    const normalizedContactsData = JSON.stringify(data);
+    await fs.writeFile(contactsPath, normalizedContactsData, "utf8");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf8");
+    return await JSON.parse(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.find(({ id }) => id === contactId);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const newContactsList = contacts.filter(({ id }) => id !== contactId);
+    writeNewContacts(newContactsList);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContactsList = [
+      ...contacts,
+      { id: uuid.v4(), name, email, phone },
+    ];
+    writeNewContacts(newContactsList);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
